0ac767bb2e958a2efa4b0dca693d452421d095ab
Massimo Imparato <33756597+Massimoic@users.noreply.github.com>
2020-03-24 20:41:16 -0500

    Add files via upload
    init

---
+++ cmake-build-debug/CMakeFiles/MaximumPointer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MaximumPointer.dir/main.cpp.obj"
+  "MaximumPointer.exe"
+  "MaximumPointer.exe.manifest"
+  "MaximumPointer.pdb"
+  "libMaximumPointer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MaximumPointer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
