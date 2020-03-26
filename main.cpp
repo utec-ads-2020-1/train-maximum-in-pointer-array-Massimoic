44de517f46d0ad7db57d543eecc91ea4c8da20a1
Massimo Imparato <massimo.imparato@utec.edu.pe>
2020-03-25 23:02:46 -0500

    update (terminado?)

--- main.cpp
+++ main.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
+int max(int *iptr[], int n) {
+    if(n <= 100) {
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            if (*iptr[i] > temp)
+                temp = *iptr[i];
+        }
+        return temp;
+    }
+}
+
 int main() {
-    cout << "Hello, World!";
+    int n, i;
+    int array[100];
+    int *iptr[100];
+    cin >> n;
+    for ( i = 0; i < n ; i++) {
+        cin >> array[(i+3) % n];
+        iptr[i] = &(array[(i+3) % n]);
+    }
+
+    cout << max(iptr, n) << endl;
     return 0;
 }
